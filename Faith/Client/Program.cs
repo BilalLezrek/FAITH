2ee1c94edaa78203f0663b9e13f3055c11a44b1e
bilal <bilallk@live.be>
2022-08-17 14:07:13 +0200

    authentication

--- Faith/Client/Program.cs
+++ Faith/Client/Program.cs
@@ -8,5 +8,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
 builder.Services.AddMudServices();
 await builder.Build().RunAsync();
